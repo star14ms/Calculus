e357853b2c4f9263b2fb57a1214f74e26da8c469
star14ms <68639278+star14ms@users.noreply.github.com>
2020-08-13 18:46:22 +0900

    Update and rename 문자열_수로_변환.py to Change_str_num.py

--- functions/Change_str_num.py
+++ functions/Change_str_num.py
@@ -2,7 +2,7 @@ from fractions import Fraction as f # 분수꼴 추가
 
 ################################################################
 
-def change_str_num(string): # 문자열을 형태별 수로 전환
+def Change_str_num(string): # 문자열을 형태별 수로 전환
   if '/' in string:
     if '.' in string:
       numerator, denominator = map(float, string.split('/'))
@@ -22,15 +22,15 @@ def change_str_num(string): # 문자열을 형태별 수로 전환
       return int(string)
       
 # +응용1
-def change_strs_nums(list): # 문자열 원소들을 형태별 수로 전환
+def Change_strs_nums(list): # 문자열 원소들을 형태별 수로 전환
   list2 = []
-  for element in list:
-    list2.append(change_str_num(element))
+  for string in list:
+    list2.append(Change_str_num(string))
   return list2
 
 ################################################################
 
-def change_str_fraction(string): # 문자열을 분수꼴로 전환
+def Change_str_fraction(string): # 문자열을 분수꼴로 전환
   if '/' in string and '.' in string:
     numerator, denominator = map(float, string.split('/'))
     decimal_digit = 0
@@ -42,15 +42,14 @@ def change_str_fraction(string): # 문자열을 분수꼴로 전환
   elif '/' in string:
     numerator, denominator = map(int, string.split('/'))
   elif '.' in string:
-    string = float(string)
-    numerator, denominator = int(str(string).replace('.','')), 10*(len(str(string))-1-str(string).index('.'))
+    numerator, denominator = int(string.replace('.','')), 10**(len(string)-1-string.index('.'))
   else: 
     numerator, denominator = int(string), 1
-  return f(numerator, denominator) 
+  return f(numerator, denominator)
 
 # 응용1
-def change_strs_fractions(list): # 문자열 원소들을 각각 분수꼴로 전환
+def Change_strs_fractions(list): # 문자열 원소들을 각각 분수꼴로 전환
   list2 = []
-  for element in list:
-    list2.append(change_str_fraction(element))
+  for string in list:
+    list2.append(Change_str_fraction(string))
   return list2
