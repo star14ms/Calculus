c13bb0c62452f4b05399449118ed2fa155d3e855
star14ms <68639278+star14ms@users.noreply.github.com>
2020-08-14 02:42:50 +0900

    Update print_human_tailored_expansion.py

--- functions/print_human_tailored_expansion.py
+++ functions/print_human_tailored_expansion.py
@@ -1,34 +1,34 @@
-# terms = 식의 각 항의 계수들 (오름차순, list)
+# coef : 식의 각 항의 계수들 (오름차순, list) (coefficients)
 
 항정보 = []  # 출력을 위해 변환시킨 항을 저장할 곳
 식 = []
 
-def 계수1_생략(terms):
-  for n in range(len(terms)):
-    if n!=0 and (terms[n] == 1 or terms[n] == -1):
-      항정보.append(str(terms[n]).replace('1',''))
+def 계수1_생략(coef):
+  for n in range(len(coef)):
+    if n!=0 and (coef[n] == 1 or coef[n] == -1):
+      항정보.append(str(coef[n]).replace('1',''))
     else:
-      항정보.append(str(terms[n]))
+      항정보.append(str(coef[n]))
       
-def x기호_지수_추가(terms):
-  for n in range(len(terms)):
+def x기호_지수_추가(coef):
+  for n in range(len(coef)):
     항정보[n] = 항정보[n] + 'x^' + str(n)
 
-def 제곱01_생략(terms):
+def 제곱01_생략(coef):
   항정보[0] = 항정보[0].replace('x^0','')
-  if len(terms)>1:
+  if len(coef)>1:
     항정보[1] = 항정보[1].replace('^1','')
   
-def 계수0_생략(terms):
-  for n in range(len(terms)):
-    if 항정보[len(terms)-1-n].find('0')==0:
-      del 항정보[len(terms)-1-n]
+def 계수0_생략(coef):
+  for n in range(len(coef)):
+    if 항정보[len(coef)-1-n].find('0')==0:
+      del 항정보[len(coef)-1-n]
 
-def Change_human_tailored_expression(terms): # 인간맞춤형 식 출력
-  계수1_생략(terms)
-  x기호_지수_추가(terms)
-  제곱01_생략(terms)
-  계수0_생략(terms)
+def Change_human_tailored_expression(coef): # 인간맞춤형 식 출력
+  계수1_생략(coef)
+  x기호_지수_추가(coef)
+  제곱01_생략(coef)
+  계수0_생략(coef)
   항정보.reverse()
   del 식[:]
   식.extend(항정보)
